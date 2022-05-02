a1be7116a2091295d3b56eb30a61430be5afb2e7
Siva-sivanesan <91198600+Siva-sivanesan@users.noreply.github.com>
2022-05-02 17:01:44 +0530

    Add project files.

---
+++ AdBooking.BAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdBooking.BAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ AdBooking.Entity/UserDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdBooking.Entity
+{
+    class UserDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public int ContactNo { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
